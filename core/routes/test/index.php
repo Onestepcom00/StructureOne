a7eb280596cf0f68534f34ed6bc52858a1c031e7
Onestepcom00 <onestepcom00@gmail.com>
2025-09-28 21:02:26 +0200

    Adaptation avec les mises a jour

--- core/routes/test/index.php
+++ core/routes/test/index.php
@@ -1,22 +1,95 @@
 <?php
 
 /**
- * Route de test - Exemple d'implémentation
+ * 
+ * ceci est un exemple complet qui utilises les fonctions globaux 
+ * 
  */
 
-// Inclure les fonctions spécifiques à cette route
+/**
+ * 
+ * Recuperer les requetes GET 
+ * 
+ */
+if($_SERVER['REQUEST_METHOD'] === 'GET'){
 
+    /**
+     * 
+     * Generer un token JWT 
+     * 
+     */
+    if(isset($_GET['id'])){
+        /**
+         * 
+         * generer le token 
+         * 
+         */
+        $token = jwt_generate($_GET['id']);
 
-// Exemple de traitement de requête
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    $result = test_function();
-    echo api_response(200, "Route test fonctionne", [
-        'message' => $result,
-        'timestamp' => date('Y-m-d H:i:s'),
-        'host' => env('DB_HOST')
-    ]);
-} else {
-    echo api_response(405, "Méthode non autorisée");
+        /**
+         * 
+         * Creer une reponse HTTP
+         * 
+         */
+        $re = [
+            "jwt_token" => $token
+        ];
+
+        /**
+         * 
+         * Afficher le token 
+         * 
+         */
+        echo api_response(200, "Token généré avec succès", $re);
+    }
+    elseif($_GET['token']){
+        /**
+         * 
+         * Recuperer le token et verifier sa validiter 
+         * 
+         */
+        $decoded = jwt_validate($_GET['token']);
+
+        if($decoded){
+            /**
+             * 
+             * Creer un tableau 
+             * 
+             */
+            $re = [
+                "jwt_decoded" => $decoded
+            ];
+
+            /**
+             * 
+             * Le code sera traiter ici car le token est valide 
+             * 
+             */
+            echo api_response(200, "Token valide", $re);
+        }else{
+            /**
+             * 
+             * Le token n'est pas valide 
+             * 
+             */
+            echo api_response(401, "Token invalide", null);
+        }
+    }
+    else{
+        /**
+         * 
+         * Aucun prametre trouver 
+         * 
+         */
+        echo api_response(400, "Aucun paramètre 'id' trouvé", null);
+    }
+}else{
+    /**
+     * 
+     * Renvoyer une erreur si la methode n'est pas autoriser
+     * 
+     */
+    echo api_response(405, "Méthode non autorisée", null);
 }
 
 ?>
